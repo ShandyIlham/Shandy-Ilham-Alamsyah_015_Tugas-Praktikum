2d045baf881fdb7f9bc570cf1ce125ccd011c0f8
Shandy Alamsyah <89757776+ShandyIlham@users.noreply.github.com>
2021-10-28 10:19:43 +0700

    Update ProgDas.Prak4.4 (Data mahasiswa).cpp

--- ProgDas.Prak4.4 (Data mahasiswa).cpp
+++ ProgDas.Prak4.4 (Data mahasiswa).cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 int main(){
-    int a[1000], b[1000], c[1000], n, jumlah1, jumlah2;
-	float rata_pertahun, rata_tk, rata_te;
+	int a[1000], b[1000], c[1000], n, jumlah1, jumlah2;
+	double rata_pertahun, rata_tk, rata_te;
  
 	cout<<"Input:\n\n";
 	cout<<"Masukan banyak data yang ingin diinput = "; cin>>n;
@@ -19,15 +19,25 @@ int main(){
   		cout<<"Teknik Komputer = "; cin>>b[i];
   		cout<<"Teknik Elektro  = "; cin>>c[i];
 	}
-  		
-	for(int i=0;i<n;i++){
+ 	
+	 system("cls");
+ 	cout<<"Output:\n\n";
+ 	cout<<"\t\t     DATA KELULUSAN MAHASISWA"<<endl;
+	cout<<"\t\t\t     2015-2016       "<<endl;
+	cout<<" ============================================================== "<<endl;
+	cout<<"| Tahun Prodi  |  Teknik  | Teknik  |  Rata-rata kelulusan per |"<<endl;
+	cout<<"|              | Komputer | Elektro |     tahun semua prodi    |"<<endl;
+	cout<<" ============================================================== "<<endl;
+	
+ 	for(int i=0;i<n;i++){
   		cout<<"| "<<setiosflags(ios::left)<<setw(13)<<a[i];
   		cout<<"| "<<setiosflags(ios::left)<<setw(9)<<b[i];
   		cout<<"| "<<setiosflags(ios::left)<<setw(8)<<c[i];
-  		rata_pertahun=(b[i]+c[i])/2;
+  		rata_pertahun=(b[i]+c[i])*0.5;
   		cout<<"| "<<setiosflags(ios::left)<<setw(24)<<rata_pertahun<<" |";
 		cout<<"\n|--------------------------------------------------------------|\n";
 	}
+	
 	jumlah1=0;
 	jumlah2=0;
 	for(int i=0;i<n;i++){
@@ -37,10 +47,13 @@ int main(){
 		rata_te=jumlah2/n;
 	}
 	
-	cout<<"|              ";
-	cout<<"| "<<setiosflags(ios::left)<<setw(9)<<rata_tk;
+	cout<<"|  Rata-rata   |          |         |                          |"<<endl;
+	cout<<"| pertahun per | "<<setiosflags(ios::left)<<setw(9)<<rata_tk;
 	cout<<"| "<<setiosflags(ios::left)<<setw(8)<<rata_te;
-
+	cout<<"|                          |"<<endl;
+	cout<<"|     prodi    |          |         |                          |"<<endl;
+	cout<<" ============================================================== "<<endl;
+	
 	getch();
 	return 0;
 }
